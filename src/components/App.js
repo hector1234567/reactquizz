f7270154824ee30b781c76831f207436e23d716d
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2023-12-01 11:59:49 +0100

    Json Server

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(function() {
+    fetch(`http://localhost:8000/questions`)
+    .then(res => res.json())
+    .then(function(data) {
+      console.log(data)
+    })
+  }, []);
   return (
     <div className="App">
     </div>
