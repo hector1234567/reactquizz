8f3d6fa089efc07dea45037a1ac31fdef55c2e7a
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2023-12-01 13:18:23 +0100

    useReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+
+const initialState = {
+  questions: [],
+  status: 'loading'
+  //loading, error, ready, active, finished
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'loadQuestions': 
+      return {...state, questions: action.payload, status: 'ready'}
+    case 'dataFailed': 
+      return {...state, status: 'error'}
+    default:
+      throw new Error('Unknown action type');
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function() {
     fetch(`http://localhost:8000/questions`)
     .then(res => res.json())
-    .then(function(data) {
-      console.log(data)
-    })
+    .then(data => 
+      dispatch({
+        type: 'loadQuestions',
+        payload: data
+      })
+    )
+    .catch(err => dispatch({type: 'dataFailed'}))
   }, []);
   return (
-    <div className="App">
-    </div>
+    <Header/>
   );
 }
 
