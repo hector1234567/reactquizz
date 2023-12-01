a068c335b073b3e8654040a3a8877af9a5fcacda
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2023-12-01 14:08:04 +0100

    Pantallas inicio, loading y error

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -9,7 +13,7 @@ const initialState = {
 
 function reducer(state, action) {
   switch(action.type) {
-    case 'loadQuestions': 
+    case 'dataReceived': 
       return {...state, questions: action.payload, status: 'ready'}
     case 'dataFailed': 
       return {...state, status: 'error'}
@@ -19,21 +23,31 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{questions, status}, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function() {
     fetch(`http://localhost:8000/questions`)
     .then(res => res.json())
     .then(data => 
       dispatch({
-        type: 'loadQuestions',
+        type: 'dataReceived',
         payload: data
       })
     )
     .catch(err => dispatch({type: 'dataFailed'}))
   }, []);
+
   return (
-    <Header/>
+    <div className="app">
+      <Header/>
+      <Main>
+        {status === 'loading' && <Loader/>}
+        {status === 'error' && <Error/>}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions}/>}
+      </Main>
+    </div>
   );
 }
 
